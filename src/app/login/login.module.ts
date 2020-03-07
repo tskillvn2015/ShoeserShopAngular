e0a2e73feed86ac98c6f3e272f24be75e7654542
Tan <Tskillvn2015@yahoo.com.vn>
2020-03-07 16:57:00 +0700

    commit Routing app

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { LoginComponent } from './login/login.component';
-
+import { LoginComponent } from './login.component';
+import {Routes,RouterModule}from '@angular/router';
+export const routers:Routes =[
+  {path:'',component:LoginComponent}
+];
 
 
 @NgModule({
   declarations: [LoginComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild(routers)
   ]
 })
 export class LoginModule { }
--- src/app/main/account/account.module.ts
+++ src/app/main/account/account.module.ts
@@ -1,13 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AccountComponent } from './account.component';
+import {Routes,RouterModule} from '@angular/router';
 
-
+const accountRoutes : Routes=[
+  {path:'',redirectTo:'index',pathMatch:'full'},
+  {path:'index',component:AccountComponent}
+]
 
 @NgModule({
   declarations: [AccountComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild(accountRoutes)
   ]
 })
 export class AccountModule { }
